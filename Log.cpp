146eee0a3b58c5a9aa2b9a3abb5e511b373709d9
johnDorian <jlessels@gmail.com>
2014-10-27 19:21:11 +0000

    added main files

---
+++ Log.cpp
@@ -0,0 +1,9 @@
+//
+//  Log.cpp
+//  
+//
+//  Created by Jason Lessels on 27/10/2014.
+//
+//
+
+#include "Log.h"
---
+++ Log.h
@@ -0,0 +1,14 @@
+//
+//  Log.h
+//  
+//
+//  Created by Jason Lessels on 27/10/2014.
+//
+//
+
+#ifndef ____Log__
+#define ____Log__
+
+#include <stdio.h>
+
+#endif /* defined(____Log__) */
